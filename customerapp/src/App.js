9cb77c78d3e913548303366923c128c9cd4df636
Banuprakash <banuprakashc@yahoo.co.in>
2023-03-13 16:57:08 +0530

    day1

--- customerapp/src/App.js
+++ customerapp/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css'; // CSS-loader and style-loader
 
 function App() {
--- customerapp/src/index.js
+++ customerapp/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom'
 import './index.css'; // css-loader, style-loder
 import App from './App'; // babel-loader
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// React version < 18 --> stack
+//ReactDOM.render(<App/>, document.getElementById('root'));
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// https://claudiopro.github.io/react-fiber-vs-stack-demo/
+// React version >= 18 --> Fiber Architecture
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+     <App />
+);
 
 
